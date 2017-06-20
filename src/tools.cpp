f909020d5813ae74993458eb18c97bab91f43a68
kcbighuge <kcbighuge@gmail.com>
2017-06-20 15:38:31 -0700

    initial commit with rmse calc

--- src/tools.cpp
+++ src/tools.cpp
@@ -12,7 +12,39 @@ Tools::~Tools() {}
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
   /**
-  TODO:
+  TO_DID:
     * Calculate the RMSE here.
   */
+  VectorXd rmse(4);
+  rmse << 0,0,0,0;
+
+	// check the validity of the following inputs:
+	// * the estimation vector size should not be zero
+	// * the estimation vector size should equal ground truth vector size
+	if (estimations.size() == 0) {
+		//cout << "Estimation vector size be zero!" << endl;
+		return rmse;
+	};
+
+	if (estimations.size() != ground_truth.size()) {
+		//cout << "Estimation and ground truth vector sizes not sames!" << endl;
+		return rmse;
+	};
+
+	//accumulate squared residuals
+	for(int i=0; i < estimations.size(); ++i){
+
+        VectorXd residual = estimations[i] - ground_truth[i];
+				residual = residual.array().square();
+        rmse += residual;
+	}
+
+	//calculate the mean
+	rmse /= estimations.size();
+
+	//calculate the squared root
+	rmse = rmse.array().sqrt();
+
+	//return the result
+	return rmse;
 }
