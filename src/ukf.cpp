017b89df7a9e24455578a1b634c3bdd72487c55d
kcbighuge <kcbighuge@gmail.com>
2017-06-21 15:23:45 -0700

    Add ProcessMeasurement function

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -24,10 +24,10 @@ UKF::UKF() {
   P_ = MatrixXd(5, 5);
 
   // Process noise standard deviation longitudinal acceleration in m/s^2
-  std_a_ = 30;
+  std_a_ = 6;  // fastest measured linear acceleration for street legal car is 0-60 mph in 2.2 second (12 m/s^2)
 
   // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = 30;
+  std_yawdd_ = 6;
 
   // Laser measurement noise standard deviation position1 in m
   std_laspx_ = 0.15;
@@ -61,11 +61,65 @@ UKF::~UKF() {}
  */
 void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
   /**
-  TODO:
-
+  TODOING:
   Complete this function! Make sure you switch between lidar and radar
   measurements.
   */
+
+  /*****************************************************************************
+  *  Initialization
+  ****************************************************************************/
+  if (!is_initialized_) {
+    // first measurement
+    cout << "UKF: " << endl;
+    ekf_.x_ = VectorXd(4);
+    ekf_.x_ << 1, 1, 1, 1;  // values are important to RMSE
+
+    if (meas_package.sensor_type_ == MeasurementPackage::RADAR) {
+      /**
+      Convert radar from polar to cartesian coordinates and initialize state.
+      */
+      float rho = meas_package.raw_measurements_[0];
+      float phi = meas_package.raw_measurements_[1];
+      float rho_dot = meas_package.raw_measurements_[2];
+
+      ekf_.x_(0) = rho * cos(phi);
+      ekf_.x_(1) = rho * sin(phi);
+      ekf_.x_(2) = rho_dot * cos(phi);
+      ekf_.x_(3) = rho_dot * sin(phi);
+    }
+    else if (meas_package.sensor_type_ == MeasurementPackage::LASER) {
+      /**
+      Initialize state.
+      */
+      ekf_.x_(0) = meas_package.raw_measurements_[0];
+      ekf_.x_(1) = meas_package.raw_measurements_[1];
+    }
+
+    previous_timestamp_ = meas_package.timestamp_;
+
+    // done initializing, no need to predict or update
+    is_initialized_ = true;
+    return;
+  }
+
+  /*****************************************************************************
+  *  Update
+  ****************************************************************************/
+  float dt = (meas_package.timestamp_ - previous_timestamp_) / 1000000.0; //in secs
+  Prediction(dt);
+
+  if (meas_package.sensor_type_ == MeasurementPackage::RADAR) {
+    // Radar updates
+    UpdateRadar(meas_package);
+
+  } else {
+    // Laser updates
+    UpdateLidar(meas_package);
+  }
+
+  previous_timestamp_ = meas_package.timestamp_;
+
 }
 
 /**
@@ -75,11 +129,17 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
  */
 void UKF::Prediction(double delta_t) {
   /**
-  TODO:
-
+  TODOING:
   Complete this function! Estimate the object's location. Modify the state
   vector, x_. Predict sigma points, the state, and the state covariance matrix.
   */
+
+  /*****************************************************************************
+  *  Create Augmented Sigma Points
+  ****************************************************************************/
+
+  //create adjusted mean state
+
 }
 
 /**
@@ -95,6 +155,8 @@ void UKF::UpdateLidar(MeasurementPackage meas_package) {
 
   You'll also need to calculate the lidar NIS.
   */
+
+  double NIS = z_diff.transpose() * S.inverse() * z_diff;
 }
 
 /**
@@ -110,4 +172,5 @@ void UKF::UpdateRadar(MeasurementPackage meas_package) {
 
   You'll also need to calculate the radar NIS.
   */
+  double NIS = z_diff.transpose() * S.inverse() * z_diff;
 }
